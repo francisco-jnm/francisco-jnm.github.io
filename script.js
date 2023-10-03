e20022832fba8deb6e519a6a34a0ae5404127e71
covid-espana <89265338+covid-espana@users.noreply.github.com>
2023-10-03 15:46:37 +0200

    2023-10-03-B

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var arr = [
     {"quote":"En lugar de darnos cuenta del sentimiento de tristeza, sentimos 'estoy triste'. </p> En lugar de darnos cuenta del sentimiento de soledad, sentimos 'estoy solo'. </p> En lugar de darnos cuenta del proceso de envejecimiento, sentimos 'me hago viejo'. </p> De esta manera, permitimos que la experiencia se apropie de nuestro ser y de su paz.", "author":"Rupert Spira"},
     {"quote":"El egoismo no consiste en vivir como uno desea, sino en pedir a los demás que vivan como uno desea vivir.", "author":"Oscar Wilde"},
 	{"quote":"Mi vida ha estado llena de terribles desgracias, la mayoría de las cuales nunca sucedieron.", "author":"Michel de Montaigne"},
-	{"quote":"El conocimiento de Uno Mismo no es una cosa a encontrar en libros, </br> ni es el resultado de una larga y dolorosa práctica o disciplina, </br> sino que es el darse cuenta, momento a momento, de cada pensamiento, emoción y movimiento a medida que surgen.", "author":"Jiddu Krishnamurti"},
+	{"quote":"El conocimiento de Uno Mismo no es una cosa a encontrar en libros, </br> ni es el resultado de una larga y dolorosa práctica o disciplina, </br> sino que es el darse cuenta, momento a momento, de cada pensamiento, emoción, sensación... a medida que surgen.", "author":"Jiddu Krishnamurti"},
 	{"quote":"Cuando dejamos ir todo lo que podemos dejar ir, lo que nunca puede abandonarnos, brilla.", "author":"Rupert Spira"}
 ];
 
