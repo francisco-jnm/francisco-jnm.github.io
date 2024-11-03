32528c9a8dfa3a7e1e60a3a96bc92928f8ad3d59
covid-espana <89265338+covid-espana@users.noreply.github.com>
2024-11-03 21:10:16 +0100

    2024-11-03-A

--- quizzes/index.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Navigate to the directory where the quiz files are located
-cd /d C:\_DOCUMENTOS_FJNM\Programacion\Javascript\Quizzes
-
-REM Start the Python local server
-start "" python -m http.server 8000
-
-REM Open the default browser to access the HTML file
-start "" http://localhost:8000/index.html
